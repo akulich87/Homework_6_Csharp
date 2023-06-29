d9e821f3af35093db694a9a779ab73d34bba2527
akulich87 <amgrupp_project@mail.ru>
2023-06-29 21:27:43 +0300

    Added Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int count = 0;
+Console.Write("Какое количество чисел, Вы желаете ввести? -> ");
+count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+int i = 0;
+int sum = 0;
+
+while(i < count)
+{
+  Console.Write($"Введите число #{i+1}: ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+  i++;
+}
+
+Console.WriteLine(String.Join(", ", array));
+
+for (int a = 0; a < array.Length; a++)
+{
+  if(array[a] >0)
+  {
+    sum += 1; 
+  }
+}
+
+Console.WriteLine($"Количество положительных чисел, равно: {sum}");
